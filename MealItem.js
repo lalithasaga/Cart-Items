15db715af16310f3a93dd63b2b213a4f90b4a148
lalithasaga <132065591+lalithasaga@users.noreply.github.com>
2023-06-12 15:49:22 +0530

    Add files via upload

---
+++ MealItem.js
@@ -0,0 +1,34 @@
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+  
+  const addToCartHandler = (amount) => {
+    // Create an item object with the selected amount
+    const item = {
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    };
+
+    // Call the add item function from CartContext
+    props.onAddToCart(item);
+  };
+
+  return (
+    <li>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
